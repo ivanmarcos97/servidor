ac0ebe9f39499b8bc807c19e8696be8c0fbabee9
ivanmarcos97 <ivanmarcos97@hotmail.com>
2021-09-30 12:22:28 +0200

    primera

---
+++ Temario/UT1/Actividades UT1/codigo_11.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> codigo 1 </title>
+</head>
+<body>
+<h1><center> !!!Bienvenidos!!!</center></h1>
+<br>
+<br>
+<?php
+	//$num=5;
+	//echo $num;
+?>
+<br>
+</html>
