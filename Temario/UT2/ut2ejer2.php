4e4772ebebe977f24d7530dca6c0e73af6a1de2b
ivanmarcos97 <ivanmarcos97@hotmail.com>
2021-10-01 14:16:18 +0200

    f

---
+++ Temario/UT2/ut2ejer2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$filas = 4;
+$simb="*";
+for($i=0;$i<$filas;$i++){
+for($y=0;$y<$i;$y++){
+    echo "$simb";
+}
+echo "<br/>";
+}
+    ?>
+</body>
+</html>
