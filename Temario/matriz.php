2f2bda4b847b63f8720615a6b78ebaef3a3700ba
ivanmarcos97 <ivanmarcos97@hotmail.com>
2021-10-07 13:07:41 +0200

    f

---
+++ Temario/matriz.php
@@ -0,0 +1,15 @@
+$comidas=array('sopa'=>1)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $comidas=array('primeros'=>array('sopa','lentejas','garbanzos'),'segundos'=>array('rape','sardinas','filete de ternera'));
+    ?>
+</body>
+</html>
