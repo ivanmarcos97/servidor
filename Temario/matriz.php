d8f8cd86824bf61cc10187b723ace057cf536964
ivanmarcos97 <ivanmarcos97@hotmail.com>
2021-10-14 12:36:26 +0200

    c

--- Temario/matriz.php
+++ Temario/matriz.php
@@ -1,4 +1,4 @@
-$comidas=array('sopa'=>1)
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +9,34 @@ $comidas=array('sopa'=>1)
 </head>
 <body>
     <?php
-        $comidas=array('primeros'=>array('sopa','lentejas','garbanzos'),'segundos'=>array('rape','sardinas','filete de ternera'));
+        $m=array(0=>array(2,3,5),
+        1=>array(3,5,7),
+        2=>array(5,5,6));
+        $sime=false;
+        for($i=0;$i<count($m);$i++){
+            for($j=0;$j<count($m[$i]);$j++){
+            echo $m[$i][$j]." ";
+            if(($m[0][1])==($m[1][0])&&
+                ($m[0][2])==($m[2][0])&&
+                ($m[1][2])==($m[2][1])){
+                    $texto="Es una matriz simetrica"; 
+            }else{$texto="";}
+            
+        }
+        echo "<br/>";
+            }
+            echo $texto;
+        
+        foreach($m as $fila){
+            $sumfila=0;
+            foreach($fila as $componente){
+            $sumfila+=$componente;}
+            $vectorfilas[]=$sumfila;}
+            for ($x=0;$x<count($vectorfilas);$x++){
+                echo $vectorfilas[$x]," ";
+            }
+        
+    
     ?>
 </body>
 </html>
