fdb590497569bd6708f5a38d6841c36fe5fdf2c4
ivanmarcos97 <ivanmarcos97@hotmail.com>
2021-11-12 14:19:51 +0100

    d

---
+++ Ejercicios/UT4 EJER/formularios/11.php
@@ -0,0 +1,29 @@
+<?php
+require_once("./servidor/libreriaphp/FUNCIONES.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo login();
+    ?>
+    <form action="" method="$_POST">
+        <fieldset>
+            <label for="a">Nombre:</label>
+            <input type="text" name="usu" id="a" required><br>
+            <label for="b">Contraseña:</label>
+            <input type="password" name="pass" id="b" required><br>
+            <input type="submit" name="enviar" value="Enviar">
+        </fieldset>
+    </form>
+</body>
+
+</html>
