35478feaa6d50403033ea3f6ad8c400d3c93c4c8
ivanmarcos97 <ivanmarcos97@hotmail.com>
2021-12-11 13:02:00 +0100

    ftfgg

--- 2EVAL/ejer/miscontroladores.php
+++ 2EVAL/ejer/miscontroladores.php
@@ -8,7 +8,7 @@ function micontrolador1($nivel, $mensaje, $fichero, $linea)
             echo "Error de advertencia: $mensaje" . " " . $fichero . " " . " en la linea $linea <br>";
             break;
         case E_NOTICE:
-            $mensaje = "No se puede usar una constante sin definir";
+            $mensaje = "No se puede usar una variable sin definir";
             echo "Error de aviso: $mensaje" . " " . $fichero . " " . " en la linea $linea <br>";
             break;
         default:
@@ -19,11 +19,11 @@ function micontrolador2($nivel, $mensaje, $fichero, $linea)
 {
     switch ($nivel) {
         case E_WARNING:
-            $mensaje = " no se puede redefinir una constante";
+            $mensaje = " error en el codigo";
             echo "Warning: $mensaje" . " " . $fichero . " " . " en la linea $linea <br>";
             break;
         case E_NOTICE:
-            $mensaje = "no se puede realizar la funcion";
+            $mensaje = " no se puede redefinir una constante";
             echo "Error de aviso $mensaje" . " " . $fichero . " " . " en la linea $linea <br>";
             break;
         default:
