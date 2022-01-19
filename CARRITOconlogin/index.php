44f4f8a49c9d112f673119afae5770ed5c2d125c
ivanmarcos97 <ivanmarcos97@hotmail.com>
2022-01-19 18:25:33 +0100

    rtee

--- CARRITOconlogin/index.php
+++ CARRITOconlogin/index.php
@@ -1,32 +1,40 @@
 <?php
-
-if (!isset($_COOKIE['login'])) {
-    $usuario = $_POST['usuario'];
-    $usuario .= "-" . $_POST['clave'] . ",";
-
-    setcookie('login', $usuario, time() + 60);
-    exit;
-} else	
-	if (isset($_POST['enviar'])) {
-    if ((!empty($_POST['usuario'])) && (!empty($_POST['clave']))) {
-        $usuario = $_COOKIE["login"];
-        $usuarioYContraseña=explode(",",$usuario);
-        foreach($usuarioYContraseña as $uyc){
-        $uyc=explode("-",$usuarioYContraseña[i])
-        
-    }
-        if(){
-
-        }
-        $usuario .= $_POST['usuario'];
+if (isset($_POST['enviar'])) {
+    if (!isset($_COOKIE['login'])) {
+        $usuario = $_POST['usuario'];
         $usuario .= "-" . $_POST['clave'] . ",";
 
         setcookie('login', $usuario, time() + 60);
+        exit;
+    } else {
 
-        
-    } else
-        echo "Acceso denegado. Las credenciales no son correctas";
-    exit;
+        if ((!empty($_POST['usuario'])) && (!empty($_POST['clave']))) {
+            $prueba = false;
+            $usuactual = "";
+            $usuario = $_COOKIE["login"];
+            $usuarioYContraseña = explode(",", $usuario);
+            foreach ($usuarioYContraseña as $uyc) {
+                $uycseparados = explode("-", $uyc);
+
+                for ($i = 0; $i < count($uycseparados); $i++) {
+                    if ($_POST['usuario'] == $uycseparados[$i] && $_POST['clave'] == $uycseparados[$i + 1]) {
+                        $prueba = true;
+                        $usuactual = $uycseparados[$i];
+                    }
+                }
+            }
+            if ($prueba == true) {
+                setcookie('login', $usuario, time() + 60);
+                echo "Bienvenido de nuevo " . $usuactual;
+            } else {
+                $usuario .= $_POST['usuario'];
+                $newusu = $_POST['usuario'];
+                $usuario .= "-" . $_POST['clave'] . ",";
+                setcookie('login', $usuario, time() + 60);
+                echo "$newusu Te  has registrado con exito";
+            }
+        }
+    }
 }
 ?>
 <!DOCTYPE html>
