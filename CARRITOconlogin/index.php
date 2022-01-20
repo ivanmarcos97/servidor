7cc9c9555b122de4b956c7a302f1abd22f106fdf
ivanmarcos97 <ivanmarcos97@hotmail.com>
2022-01-20 14:19:58 +0100

    efe

--- CARRITOconlogin/index.php
+++ CARRITOconlogin/index.php
@@ -2,9 +2,11 @@
 if (isset($_POST['enviar'])) {
     if (!isset($_COOKIE['login'])) {
         $usuario = $_POST['usuario'];
+        $usuarioactual = $_POST['usuario'];
         $usuario .= "-" . $_POST['clave'] . ",";
 
         setcookie('login', $usuario, time() + 60);
+        echo "Bienvenido " . $usuarioactual;
         exit;
     } else {
 
@@ -42,22 +44,51 @@ if (isset($_POST['enviar'])) {
 
 <head>
     <meta charset="utf-8">
-    <title> SERIALIZACIÓN DE OBJETOS </title>
+    <title> Login </title>
+    <style>
+        body {
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center;
+
+
+            background-color: lightgoldenrodyellow;
+
+
+        }
+
+        h1 {
+            border-bottom: dashed 2px darkgoldenrod;
+            padding: 10px;
+        }
+
+        form {
+            font-size: 1.5em;
+        }
+
+        #login {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 20px;
+        }
+    </style>
 </Head>
 
 <body>
+    <main>
+        <br>
+        <!--FORMULARIO -->
+        <p align="center">
+        <h1> Inicio de sesión </h1>
+        <form action="" method="post">
+            Usuario: <input type="text" name="usuario" placeholder="Introducca su Usuario" required> <br>
+            Contraseña: <input type="password" name="clave" placeholder="Introducca su Contraseña" required>
 
-    <br>
-    <!--FORMULARIO -->
-    <p align="center">
-    <h2> credenciales del usuario </h2>
-    <form action="" method="post">
-        Usuario: <input type="text" name="usuario">
-        Contraseña: <input type="password" name="clave">
-        <input type="submit" name="enviar" value="LOGIN">
-    </form>
-    </p>
-
+            <input id="login" type="submit" name="enviar" value="LOGIN">
+        </form>
+        </p>
+    </main>
 </body>
 
 </html>
